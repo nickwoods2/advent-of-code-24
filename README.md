05c9d91c6bdc9098226616213ce9109442290856
nickwoods2 <nick.woods@deliveroo.com>
2024-12-02 11:48:47 +0000

    first

---
+++ README.md
@@ -0,0 +1,27 @@
+# Advent of code 2024 in C++
+
+## Run
+
+1. Create a dir to compile code
+
+```
+mkdir build
+cd build 
+```
+
+2. Run CMake to generate makefiles
+
+```
+cmake ..
+```
+
+3. Compile
+
+```
+make
+```
+
+4. Execute
+```
+./dayXX
+```
---
+++ build/CMakeFiles/day02.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/day02.dir/src/day02/main.cpp.o"
+  "CMakeFiles/day02.dir/src/day02/main.cpp.o.d"
+  "day02"
+  "day02.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/day02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
